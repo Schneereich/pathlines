a94980b67a8cbb920e08d333770e8b840c0571a8
Sean Schneeweiss <seansnowwhite@aol.com>
2020-02-01 16:33:37 +0100

    initial commit

---
+++ bahn.m
@@ -0,0 +1,68 @@
+function [x,y] = bahn(x0,y0,T,N,Z,U,V,X,Y)
+
+dt = T/N; %Zeitschrittweite
+h = dt/(Z+1); %zusätzliche Punkte zwischen den Zeitschritten
+x = zeros(1,N+Z(N-1));
+y = zeros(1,N+Z(N-1));
+
+x(1) = x0;
+y(1) = y0;
+
+% Zeitpunke, an denen die Position des Partikels bestimmt wird
+t = linspace(0,T,N+Z(N-1));
+
+for k = 1:length(t)-1
+
+    tp = floor(t(k)/dt)+1;
+    tn = ceil(t(k)/dt)+1;
+    gamma = (t(k)-tp)/dt;
+    
+    if gamma ~= 0 % zwischen den Knotenpunkten
+        
+        [up,vp] = evalVelocity(X,Y,U{tp},V{tp},x(k),y(k));
+        [un,vn] = evalVelocity(X,Y,U{tn},V{tn},x(k),y(k)); % Verfahrensfehler durch Approx.
+        uk = up + gamma*(un-up);
+        vk = vp + gamma*(vn-vp);
+        
+    else % auf Knotenpunkt
+        
+       [uk,vk] = evalVelocity(X,Y,U{tp},V{tp},x(k),y(k));
+
+    end
+    
+    if isnan(uk)
+        if y(k) > Y(end,1) %über Ymax Grenze drüber geschossen
+            s = (y(k)-y(k-1))/(x(k)-x(k-1));
+            x(k) = x(k-1) + (Y(end,1)-y(k-1))/s;
+            y(k) = Y(end,1);
+        elseif x(k) > X(1,end) %über Xmax Grenze drüber geschossen
+            s = (y(k)-y(k-1))/(x(k)-x(k-1));
+            y(k) = s*(X(1,end)+x(k-1)) + y(k-1);
+            x(k) = X(1,end);
+        else %über Ymin Grenze drüber geschossen
+            s = (y(k)-y(k-1))/(x(k)-x(k-1));
+            x(k) = x(k-1) + (Y(1,1)-y(k-1))/s;
+            y(k) = Y(1,1);          
+        end
+        % x, y Vektor - Reste abschneiden
+        x = x(1:k);
+        y = y(1:k);
+        return
+    end
+    
+    if isinf(uk)
+        % x, y Vektor - Reste abschneiden
+        x = x(1:k);
+        y = y(1:k);
+        return
+    end
+    
+    % Euler
+    x(k+1) = x(k) + h * uk;
+    y(k+1) = y(k) + h * vk;
+    
+
+end
+end
+
+
---
+++ evalVelocity.m
@@ -0,0 +1,45 @@
+function [uk,vk] = evalVelocity(X,Y,U,V,xk,yk)
+
+% evtl. global definieren
+m = size(X,2);
+n = size(X,1);
+dx = X(1,end)/m;
+dy = Y(end,1)/n;
+
+%% find element
+leftIdx = floor(xk/dx)+1; %Indizes
+rightIdx = ceil(xk/dx)+1;
+downIdx = floor(yk/dy)+1;
+upIdx = ceil(yk/dy)+1;
+
+% Fallunterscheidung
+if leftIdx < 1 || rightIdx > m || downIdx < 1 || upIdx > n
+    warning("Out of boundaries.");
+    uk = NAN;
+    vk = NAN;
+    return
+end
+
+if any(isnan([U(leftIdx,downIdx),U(leftIdx,upIdx),U(rightIdx,downIdx),U(rightIdx,upIdx)]))
+    warning("Velocity is infinity.");
+    uk = inf;
+    vk = inf;
+    return
+end
+    
+alpha = (xk-leftIdx)/dx;
+beta = (yk-downIdx)/dy;
+
+
+%% interpolation
+u1 = U(leftIdx,downIdx)+alpha*(U(rightIdx,downIdx)-U(leftIdx,downIdx));
+u2 = U(leftIdx,upIdx)+alpha*(U(rightIdx,upIdx)-U(leftIdx,upIdx));
+v1 = V(leftIdx,downIdx)+alpha*(V(rightIdx,downIdx)-V(leftIdx,downIdx));
+v2 = V(leftIdx,upIdx)+alpha*(V(rightIdx,upIdx)-V(leftIdx,upIdx));
+
+
+uk = u1+beta*(u2-u1);
+vk = v1+beta*(v2-v1);
+
+end
+
