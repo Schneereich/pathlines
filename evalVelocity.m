c924fb3760d45d79e4c7d744bdba3ff6d4e206d8
Sean Schneeweiss <seansnowwhite@aol.com>
2020-02-12 11:19:17 +0100

    docs: comment the scripts

--- evalVelocity.m
+++ evalVelocity.m
@@ -1,41 +1,60 @@
 function [uk,vk] = evalVelocity(X,Y,U,V,xk,yk)
+%% evalVelocity  Return velocities
+%
+% Assumptions: tbd.
+%
+% Inputs:
+%   xk, yk coordinates of evaluated point
+%   X, Y in R^(n x m) equidistant grid
+%   U, V cell-array with N entries as velocity profile
+% Outputs:
+%   uk, vk velocities of particle
 
-% evtl. global definieren
+%
+% $Date: February 5, 2020
+% ________________________________________
+
+% maye define this variables globally
 m = size(X,2);
 n = size(X,1);
 dx = X(1,end)/m;
 dy = Y(end,1)/n;
 
-%% find element
-leftIdx = floor(xk/dx)+1; %Indizes
+%% find element indices
+leftIdx = floor(xk/dx)+1;
 rightIdx = ceil(xk/dx)+1;
 downIdx = floor(yk/dy)+1;
 upIdx = ceil(yk/dy)+1;
 
-% Fallunterscheidung
-if leftIdx < 1 || rightIdx > m || downIdx < 1 || upIdx > n %auﬂerhalb des Gitters
-    warning("Out of boundaries.");
-    uk = NAN;
-    vk = NAN;
+%% case distinction
+% case 1: particle outside of grid
+if leftIdx < 1 || rightIdx > m || downIdx < 1 || upIdx > n
+    warning("Particle is out of grid boundaries. Setting velocity to NaN");
+    uk = NaN;
+    vk = NaN;
     return
 end
 
-if any(isnan([U(leftIdx,downIdx),U(leftIdx,upIdx),U(rightIdx,downIdx),U(rightIdx,upIdx)])) %im Loch
-    warning("Velocity is infinity.");
+% case 2: particle inside inner circle
+if any(isnan([U(leftIdx,downIdx),U(leftIdx,upIdx), ...
+              U(rightIdx,downIdx),U(rightIdx,upIdx)]))
+    warning(["Particle is inside inner circle." ...
+             "Setting velocity to infinity."]);
     uk = inf;
     vk = inf;
     return
 end
     
+% case 3: particle position valid, interpolate values
 alpha = (xk-X(1,leftIdx))/dx;
-beta = (yk-Y(downIdx,1))/dy;
+beta  = (yk-Y(downIdx,1))/dy;
 
 
-%% interpolation
+%% interpolation between edge values
 u1 = U(leftIdx,downIdx)+alpha*(U(rightIdx,downIdx)-U(leftIdx,downIdx));
-u2 = U(leftIdx,upIdx)+alpha*(U(rightIdx,upIdx)-U(leftIdx,upIdx));
+u2 = U(leftIdx,  upIdx)+alpha*(U(rightIdx,  upIdx)-U(leftIdx,  upIdx));
 v1 = V(leftIdx,downIdx)+alpha*(V(rightIdx,downIdx)-V(leftIdx,downIdx));
-v2 = V(leftIdx,upIdx)+alpha*(V(rightIdx,upIdx)-V(leftIdx,upIdx));
+v2 = V(leftIdx,  upIdx)+alpha*(V(rightIdx,  upIdx)-V(leftIdx,  upIdx));
 
 
 uk = u1+beta*(u2-u1);
